39cee6882abc68f3d60ed2dc6d93526a1e4b54e7
d3xt3er <48564999+d3xt3er@users.noreply.github.com>
2020-08-19 17:10:16 -0300

    Portfolio
    Biblioteca lightbox adicionada

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function ($) {
-    $('.scroll').click(function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 700);
-    });
-});
